d3aa6ffce69b0088ec0163ffa78670c8963737a7
levan2007 <levani200711@gmail.com>
2023-03-06 01:51:11 -0800

    added style

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const get_joke = document.getElementById("get_joke");
-get_joke.addEventListener('click' , generateJokes);
+const jokeEL = document.getElementById('joke')
+const get_joke = document.getElementById('get_joke')
 
-generateJokes();
+get_joke.addEventListener('click' , generatejoke)
 
-async function generateJokes(){
-    const jokeEL = document.getElementById("joke");
-    const jokeData = await fetch('https://api.chucknorris.io/jokes/random' , {
+generatejoke();
+
+async  function generatejoke(){
+    const jokeRes = await fetch('https://api.chucknorris.io/jokes/random' ,
+    {
         headers:{
             accept: 'application/json'
         }
     });
-    const jokes = await jokeData.json();
-    console.log(jokes); 
-    jokeEL.innerHTML = jokes.value;
+    const joke = await jokeRes.json();
+    console.log(joke)
+    jokeEL.innerHTML = joke.value;
 }
